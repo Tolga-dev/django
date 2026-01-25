6168c7f77c5a7864b8ebd44c9ad20e0c92015329
Tolga Konat <tolga.konat.12@gmail.com>
2026-01-25 10:29:31 +0300

    Implement Data Store

--- src/cfehome/view.py
+++ src/cfehome/view.py
@@ -2,13 +2,18 @@ from django.http import HttpResponse
 from django.shortcuts import render 
 import pathlib
 
+from visits.models import PageVisit
+
 curr_dir = pathlib.Path(__file__).resolve().parent
 
 def home_page_view(req, *args, **kwargs):
+    queryset = PageVisit.objects.all()
     my_title = "My page"
     my_context = {
-        "page_title" : my_title
+        "page_title" : my_title,
+        "page_visit_count": queryset.count()
     } 
+    PageVisit.objects.create()
     html_template = "home.html"
     return render(req, html_template, my_context)
 
